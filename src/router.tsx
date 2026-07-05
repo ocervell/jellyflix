11a20656dde5253f3c0516c22d8dc519ee548c70
Olivier Cervello <ocervello@freelabz.com>
2026-07-05 23:51:18 +0200

    feat: /search route (grid + sort/status filters + DetailModal)

--- src/router.tsx
+++ src/router.tsx
@@ -3,11 +3,13 @@ import Login from './routes/Login';
 import Home from './routes/Home';
 import Watch from './routes/Watch';
 import Library from './routes/Library';
+import Search from './routes/Search';
 import RequireAuth from './components/common/RequireAuth';
 
 export const router = createHashRouter([
   { path: '/login', element: <Login /> },
   { path: '/', element: <RequireAuth><Home /></RequireAuth> },
   { path: '/library/:viewId', element: <RequireAuth><Library /></RequireAuth> },
+  { path: '/search', element: <RequireAuth><Search /></RequireAuth> },
   { path: '/watch/:itemId', element: <RequireAuth><Watch /></RequireAuth> },
 ]);
