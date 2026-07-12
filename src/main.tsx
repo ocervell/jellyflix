c7ebce7d1bfdefd59124994393cccb3f57928fa9
Olivier Cervello <ocervello@freelabz.com>
2026-07-12 11:44:32 +0200

    feat(tv): global Back handler stack (useTvBack)

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import { ApiProvider } from './hooks/useApi';
 import { initFocus } from './lib/tv/focus';
+import { TvBackProvider } from './lib/tv/back';
 import './styles/reset.css';
 import './styles/tokens.css';
 
@@ -17,7 +18,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ApiProvider>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <TvBackProvider>
+          <App />
+        </TvBackProvider>
       </QueryClientProvider>
     </ApiProvider>
   </StrictMode>,
